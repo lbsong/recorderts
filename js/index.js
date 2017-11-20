7851672598c07826a4f5a5581452460d3d3aa1ec
Libin Song <v-lsong@microsoft.com>
2017-11-19 23:12:38 -0800

    new

---
+++ js/index.js
@@ -0,0 +1,9 @@
+import {Recorder} from './js/recorder.js';
+
+let worker = new Worker('./worker.js');
+
+let recorder = new recorder();
+
+document.onload(event => {
+    let 
+});
---
+++ recorder.ts
@@ -0,0 +1,57 @@
+class Recorder {
+    worker: Worker;
+    context: AudioContext;
+    microphone: MediaStreamAudioSourceNode;
+    processor: ScriptProcessorNode;
+
+    recording = false;
+
+    constructor(worker) {
+        let _this = this;
+
+        this.worker = worker;
+        this.worker.onmessage = function (event) {
+            switch (event.data.command) {
+                case 'abc':
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        navigator.mediaDevices.getUserMedia({ audio: true })
+            .then(stream => {
+                _this.context = new AudioContext();
+                _this.microphone = _this.context.createMediaStreamSource(stream);
+                _this.processor = _this.context.createScriptProcessor(4096, 1, 1);
+
+                _this.processor.onaudioprocess = function (event) {
+                    let buffer = event.inputBuffer.getChannelData(0);
+
+                    _this.worker.postMessage({
+                        command: 'encode',
+                        buffer: buffer
+                    });
+                };
+            })
+            .catch(reason => {
+                console.log(reason);
+            });
+    }
+
+    start() {
+        this.microphone.connect(this.processor);
+        this.processor.connect(this.context.destination);
+
+        console.log("started");
+    }
+
+    stop() {
+        this.microphone.disconnect();
+        this.processor.disconnect();
+
+        console.log("stopped");
+    }
+
+    function 
+}
