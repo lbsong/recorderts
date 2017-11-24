b702171b365d51ea43ea759bc8f411e22be25603
lbsong <v-lsong@microsoft.com>
2017-11-24 06:38:46 -0800

    remove unnecessary files

--- workers/xfworker.js
+++ workers/xfworker.js
@@ -1,24 +1,11 @@
-/// <reference path="../node_modules/@types/jquery/index.d.ts"/>
-
 (function () {
     'use strict';
-    
-    //importScripts("http://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.2.1.js");
 
     function RecognizeSpeech(buffer) {
         let blob = new Blob(buffer);
 
         let formData = new FormData();
         formData.append("buffer", blob);
-
-        // $.ajax({
-        //     type: "POST",
-        //     url: "api/rec",
-        //     data: formData,
-        //     success: function (data) {
-        //         console.log("post succeeded!");
-        //     }
-        // });
 
         let xhr = new XMLHttpRequest();
         xhr.open("POST", "http://localhost:57591/api/rec", true);
